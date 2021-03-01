fce7584c80bb7f212633ae343829d2453984dedf
tikemajson <59565516+tikemajson@users.noreply.github.com>
2021-03-01 08:23:48 +0100

    Add files via upload

--- NBPCurrencyRates/src/module-info.java
+++ NBPCurrencyRates/src/module-info.java
@@ -6,6 +6,7 @@ module NBPCurrencyRates {
 	requires org.hibernate.orm.core;
 	requires java.persistence;
 	requires java.sql;
+	requires junit;
 	requires nv.i18n;
 	exports nbpcurrencyrates.service;
 	opens nbpcurrencyrates.service;
